45e3de9ce20cdf9c4a0648ef9518de43007b40bf
jmj <jeremyj@letifer.org>
2011-07-09 21:34:40 -0500

    Initial boilerplate

---
+++ src/rabt2/Blastoff.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rabt2;
+
+/**
+ *
+ * @author jjones
+ */
+public class Blastoff implements Runnable {
+
+    private int count;
+    
+    public Blastoff() {
+        count = 5;
+        
+    }
+    
+    @Override
+    public void run() {
+        
+    }
+    
+}
