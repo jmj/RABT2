7e2157551fa3a072283fb456068c0f32353af44e
jmj <jeremyj@letifer.org>
2011-07-09 21:39:41 -0500

    Implement run()

--- src/rabt2/Blastoff.java
+++ src/rabt2/Blastoff.java
@@ -9,16 +9,13 @@ package rabt2;
  * @author jjones
  */
 public class Blastoff implements Runnable {
-
-    private int count;
-    
-    public Blastoff() {
-        count = 5;
-        
-    }
     
     @Override
     public void run() {
+        for (int count=5;count>0; count--) {
+            System.out.format("%s: %d\n", Thread.currentThread().getName(),
+                    count);
+        }
         
     }
     
