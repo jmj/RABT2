cd014806723550d2db477a2a010600f59c1bd0ef
jmj <jeremyj@letifer.org>
2011-07-09 21:45:50 -0500

    Create and execute 5 threads

--- src/rabt2/RABT2.java
+++ src/rabt2/RABT2.java
@@ -14,6 +14,16 @@ public class RABT2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Thread thread1 = new Thread(new Blastoff(), "Thread 1");
+        Thread thread2 = new Thread(new Blastoff(), "Thread 2");
+        Thread thread3 = new Thread(new Blastoff(), "Thread 3");
+        Thread thread4 = new Thread(new Blastoff(), "Thread 4");
+        Thread thread5 = new Thread(new Blastoff(), "Thread 5");
+        
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+        thread5.start();
     }
 }
